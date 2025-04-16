c29466772ed7284b93dc15d0a81c329e94db3926
yite <yite@icloud.com>
2025-04-16 09:18:10 +0800

    update 0.3.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="deribit-python",
-    version="0.2.0",
+    version="0.3.0",
     author="yite",
     author_email="coastq22889@icloud.com",
     description="A Python wrapper for the Deribit cryptocurrency exchange API",
